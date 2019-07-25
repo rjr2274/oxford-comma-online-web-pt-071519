4ac852c3ddae59d1e62a972f99a5e1b71179b75f
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-25 15:53:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length < 3
+    array.join(" and ")
+  elsif
+    last_word = array.pop
+    new_string = array.join(", ") + ", and " + last_word
+    return new_string
+  end
 end
