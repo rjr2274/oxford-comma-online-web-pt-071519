d06e4c45298bce56318077ab68a76527cbdf2abb
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-25 15:44:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-  if array.length > 3
+  if array.length < 2
     array.join(" and ")
+  elsif array.length > 2
+    last_word = array.pop
+    array.join(", ") + "and " + last_word
+  end
+    
 end
