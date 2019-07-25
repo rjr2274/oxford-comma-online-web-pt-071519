733be63f8db6e8adc918a35589a135f87f81b911
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-25 15:46:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2
     last_word = array.pop
-    array.join(", ") + "and " + last_word
+    return array.join(", ") + "and " + last_word
   end
 
 end
